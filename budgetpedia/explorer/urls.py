02c686360b8fba0de69d554c733f3a6b2602fa92
asterix135 <chgraham@gmail.com>
2016-11-06 17:02:27 -0500

    additional sample pages added

--- budgetpedia/explorer/urls.py
+++ budgetpedia/explorer/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^process/$', views.process, name='process'),
+    url(r'^impact/$', views.impact, name='impact'),
+    url(r'^detail/$', views.detail, name='detail'),
 ]
--- budgetpedia/explorer/views.py
+++ budgetpedia/explorer/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'explorer/index.html')
+
+
+def process(request):
+    return render(request, 'explorer/process.html')
+
+
+def impact(request):
+    return render(request, 'explorer/impact.html')
+
+
+def detail(request):
+    return render(request, 'explorer/detail.html')
