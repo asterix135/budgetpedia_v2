eb41c16d388bbccce1ffaa855707e03b638cda9e
asterix135 <chgraham@gmail.com>
2016-11-25 08:34:53 -0500

    basic (non-django) sql interface ok

--- database_stuff/import_tree.py
+++ database_stuff/import_tree.py
@@ -11,4 +11,14 @@ def connect():
                                  db='budgetpedia')
     return connection
 
-connect()
+def main():
+    connection = connect()
+    with connection.cursor() as cursor:
+        sql = 'SELECT * from explorer_lineitem'
+        cursor.execute(sql)
+        results = cursor.fetchall()
+    connection.close()
+    print(len(results))
+
+if __name__ == '__main__':
+    main()
