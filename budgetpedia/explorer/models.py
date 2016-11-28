d49ffdeafeb941d71584c8ac6b7222a8028fa6dc
asterix135 <chgraham@gmail.com>
2016-11-27 22:05:22 -0500

    updated data model

--- budgetpedia/explorer/models.py
+++ budgetpedia/explorer/models.py
@@ -1,35 +1,43 @@
 from django.db import models
 
-class LineItem(models.Model):
+class Municipality(models.Model):
+    """ Contains info on municipalities available """
+    municipal_name = models.CharField(max_length=100)
+    fir_name = models.CharField(max_length=100)  # Name from FIR Data
+    municipal_id = models.IntegerField(unique=True)
+
+
+class Node(models.Model):
+    """ Main Node Information for Budget Tree """
     code = models.CharField(max_length=25)
     description = models.TextField(blank=True)
     parent = models.ForeignKey('self')
     item_type = models.CharField(max_length=6)
+    municipality = models.ForeignKey(Municipality)
+
+
+class Keywords(models.Model):
+    """ Tags (user-generated?) for identifying nodes beyond description """
+    keyword = models.CharField(max_length=50)
+    detail = models.ManyToManyField(Node)
+
+
+class EntryDetails(models.Model):
+    """ Data for individual node instance """
+    category = models.ForeignKey(Node)
+    year = models.PositiveSmallIntegerField()
+    amount = models.IntegerField()
+    comments = models.TextField()
+
+
+class ClosureTable(models.Model):
+    """ Used to keep tree information in SQL format """
+    ancestor = models.ForeignKey(Node, related_name='ancestor')
+    descendant = models.ForeignKey(Node, related_name='descendant')
+    depth = models.IntegerField()
+
 
 # Closure model for Modeling tree to SQL
 # http://mikehillyer.com/articles/managing-hierarchical-data-in-mysql/
 # http://www.slideshare.net/billkarwin/models-for-hierarchical-data
 # https://coderwall.com/p/lixing/closure-tables-for-browsing-trees-in-sql
-
-
-class Keywords(models.Model):
-    keyword = models.CharField(max_length=50)
-    detail = models.ManyToManyField(LineItem)
-
-
-class CostDetails(models.Model):
-    category = models.ForeignKey(LineItem)
-    year = models.PositiveSmallIntegerField()
-    amount = models.IntegerField()
-    multiplier = models.IntegerField()
-    currency = models.CharField(max_length=15)
-
-
-class Muncipality(models.Model):
-    municipality = models.CharField(max_length=100)
-
-
-class ClosureTable(models.Model):
-    ancestor = models.ForeignKey(LineItem, related_name='ancestor')
-    descendant = models.ForeignKey(LineItem, related_name='descendant')
-    depth = models.IntegerField()
